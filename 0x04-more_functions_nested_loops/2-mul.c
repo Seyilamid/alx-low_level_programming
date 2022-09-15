b56f5503ac2b24f9d3f4625fed01cc13cea08879
Oluwaseyi Adekunle Lamid <111152550+Seyilamid@users.noreply.github.com>
2022-09-15 22:26:17 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - checks uppercase
- * @c: parameter
- * Return: 0 if sucessful
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something
  */
 
-int _isupper(int c)
+int mul(int a, int b)
 {
-	if (c >= 65 && c < 91)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (a * b);
 }
