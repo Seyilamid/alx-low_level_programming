eff25675dfe58e1710b042996aaa81c640e04653
Oluwaseyi Adekunle Lamid <111152550+Seyilamid@users.noreply.github.com>
2022-09-15 22:18:22 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,2 +1,19 @@
+#include "main.h"
 
-ÂÂ#include
+/**
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if sucessful
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
