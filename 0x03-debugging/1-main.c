acb8d3ebdb7354e9f6f669ea92c1480558207967
Seyilamid <seyilamid@gmail.com>
2022-09-15 12:49:02 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+
+/**
+
+* main - prints the largest of 3 integers
+
+* Return: 0
+
+*/
+
+
+
+int main(void)
+
+{
+int 1;
+
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*
+while (i < 10)
+*{
+putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
