ebfb40f509e22c1f0952f5325a16dafb87431517
Oluwaseyi Adekunle Lamid <111152550+Seyilamid@users.noreply.github.com>
2022-09-21 21:17:04 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,3 @@
-#include "main.h"
-/**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will also be changed
- *Return: dest
- */
-
 char *_strcat(char *dest, char *src)
 {
 int i, j;
