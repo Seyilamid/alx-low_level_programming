e1e760c496797ccfa8a981ef281fd9c35d380e39
Seyilamid <seyilamid@gmail.com>
2022-09-22 12:07:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,1 @@
-#include "main.h"
-
-/**
- * print_alphabet - function to print abc
- *
- * Return: Always 0.
- */
-void print_alphabet(void)
-{
-	char c;
-	for (c = "a"; c <= "z"; c++)
-	{
-		_putcha(c);
-	}
-_putcha('\n');
-}
+uuu
